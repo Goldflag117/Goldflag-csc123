4dbb2ddf73704bc54b7c5efad3b82117e045aad8
creep <creep@LAPTOP-HOKMVFG7.fios-router.home>
2023-10-23 15:58:50 -0700

    my stuff

--- CSC123-F23/src/csc/BookTest.java
+++ CSC123-F23/src/csc/BookTest.java
@@ -5,7 +5,7 @@ public class BookTest {
 	public static void main(String[] args) {
 		Library library1 = new Library("snt jude","180th street","city","CA");
 		Library library2 = new Library("snt null","140th street","nonia","PA");
-		library1.setBook(myBook);
+		//library1.setBook(myBook);
 		System.out.println(library1);
 		System.out.println(library2);
 
